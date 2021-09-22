50ca40cbacc3a81b1b35a4df4834e6765581ada0
nothking <king520025@foxmail.com>
2021-09-22 22:08:06 +0800

    change

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,26 +1,26 @@
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadTest {
 
-    static int num = 0;
+    static AtomicInteger num = new AtomicInteger(0);
 
     public static void main(String[] args) throws Exception {
-        
+
         List<Thread> list = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            list.add(new Thread(()->{
-                for(int x = 0;x <10000 ; x++){
-                    num++;
+            list.add(new Thread(() -> {
+                for (int x = 0; x < 1000000; x++) {
+                    num.incrementAndGet();
                 }
             }));
         }
-        for(Thread t :list){
+        for (Thread t : list) {
             t.start();
         }
 
         Thread.sleep(1000L);
         System.out.println(num);
     }
-
 
 }
